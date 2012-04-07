b62b1cb4b2ecddb0f39ef69841b697bf2577c1bd
the.dylan.price <the.dylan.price@localhost>
2012-04-06 20:37:02 -0700

    Changed lastSyncTime create sql to TEXT.

---
+++ src/yoonsung/odk/spreadsheet/Activity/util/LanguageUtil.java
@@ -0,0 +1,37 @@
+package yoonsung.odk.spreadsheet.Activity.util;
+
+import yoonsung.odk.spreadsheet.data.TableProperties;
+import yoonsung.odk.spreadsheet.data.TableViewSettings;
+
+
+public class LanguageUtil {
+    
+    public static String getTableTypeLabel(int tableType) {
+        switch (tableType) {
+        case TableProperties.TableType.DATA:
+            return "Data";
+        case TableProperties.TableType.SECURITY:
+            return "Access Control";
+        case TableProperties.TableType.SHORTCUT:
+            return "Shortcut";
+        default:
+            throw new RuntimeException("Invalid table type (" + tableType +
+                    ").");
+        }
+    }
+    
+    public static String getViewTypeLabel(int viewType) {
+        switch (viewType) {
+        case TableViewSettings.Type.TABLE:
+            return "Table";
+        case TableViewSettings.Type.LIST:
+            return "List";
+        case TableViewSettings.Type.LINE_GRAPH:
+            return "Line Graph";
+        case TableViewSettings.Type.BOX_STEM:
+            return "Box-Stem Graph";
+        default:
+            throw new RuntimeException();
+        }
+    }
+}
