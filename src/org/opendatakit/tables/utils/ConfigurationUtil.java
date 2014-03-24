2e0b9dfd77d8c11984a8d88037fcbe4993385c5d
Sam Sudar <sudar.sam@gmail.com>
2014-03-24 15:33:23 -0700

    changed the icon.

---
+++ src/org/opendatakit/tables/utils/ConfigurationUtil.java
@@ -0,0 +1,36 @@
+package org.opendatakit.tables.utils;
+
+import java.io.File;
+
+import org.opendatakit.tables.data.Preferences;
+
+/**
+ * Various utilities for initial configuration of tables
+ * @author June
+ *
+ */
+public class ConfigurationUtil {
+
+    /**
+     * @return true if the config.properties file has been changed since the
+     * configuration initialization
+     */
+	public static boolean isChanged(Preferences prefs) {
+
+		if (TableFileUtils.tablesConfigurationFileExists(prefs.getAppName())) {
+		  File configFile =
+		      new File(TableFileUtils.getTablesConfigurationFile(prefs.getAppName()));
+			long timeLastConfig = prefs.getTimeLastConfig();
+			long timeLastMod = configFile.lastModified();
+			if (timeLastMod == timeLastConfig)
+				return false;
+			else
+				return true;
+		} else
+			return false;
+	}
+
+	public static void updateTimeChanged(Preferences prefs, long timeLastConfig) {
+		prefs.setLastTimeConfig(timeLastConfig);
+	}
+}
