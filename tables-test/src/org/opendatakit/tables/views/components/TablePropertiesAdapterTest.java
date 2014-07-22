45404dde99603d983b72d892d669eac78207c198
mitchellsundt <mitchellsundt@gmail.com>
2014-07-22 10:24:21 -0700

    Remove is_unit_of_retention from db and REST interface.

---
+++ tables-test/src/org/opendatakit/tables/views/components/TablePropertiesAdapterTest.java
@@ -0,0 +1,146 @@
+package org.opendatakit.tables.views.components;
+
+import static org.fest.assertions.api.ANDROID.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.robolectric.Robolectric.shadowOf;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.opendatakit.common.android.data.TableProperties;
+import org.opendatakit.tables.R;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowDrawable;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+@RunWith(RobolectricTestRunner.class)
+public class TablePropertiesAdapterTest {
+
+  private TablePropertiesAdapter mAdapter;
+
+  @Before
+  public void setup() {
+    TableProperties tp1 = mock(TableProperties.class);
+    TableProperties tp2 = mock(TableProperties.class);
+    when(tp1.getLocalizedDisplayName()).thenReturn("alpha");
+    when(tp2.getLocalizedDisplayName()).thenReturn("beta");
+    List<TableProperties> listOfMocks = new ArrayList<TableProperties>();
+    listOfMocks.add(tp1);
+    listOfMocks.add(tp2);
+    this.mAdapter = new TablePropertiesAdapter(listOfMocks);
+  }
+
+  @Test
+  public void testGetCount() {
+    assertThat(mAdapter).hasCount(2);
+  }
+
+  @Test
+  public void getView_firstItemCorrectText() {
+    View view = this.getView(0, null);
+    assertThat(view)
+      .isNotNull()
+      .isVisible();
+    TextView textView = (TextView) view.findViewById(R.id.row_item_text);
+    assertEquals("alpha", textView.getText().toString());
+  }
+
+  @Test
+  public void getView_secondItemCorrectText() {
+    View view = this.getView(1, null);
+    assertThat(view)
+      .isNotNull()
+      .isVisible();
+    TextView textView = (TextView) view.findViewById(R.id.row_item_text);
+    assertEquals("beta", textView.getText().toString());
+  }
+
+  @Test
+  public void getView_shouldRecycleViews() {
+    LayoutInflater layoutInflater = (LayoutInflater) Robolectric
+        .application
+        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    RelativeLayout existingView = (RelativeLayout)
+        layoutInflater.inflate(R.layout.row_item_with_preference,
+        null);
+    View view = this.getView(0, existingView);
+    assertThat(view).isSameAs(existingView);
+  }
+
+  @Test
+  public void getView_iconClickPerformsLongClickOnParent() {
+    View view = this.getView(1, null);
+    ImageView icon = (ImageView) view.findViewById(R.id.row_item_icon);
+    // We're going to test that this works just by adding a longClick listener.
+    // Because we have to use final, we can't do a simple ++. Going to modify
+    // an object.
+    final String key = "key";
+    final Map<String, Integer> buttonClicks = new HashMap<String, Integer>();
+    buttonClicks.put(key, 0);
+    view.setOnLongClickListener(new View.OnLongClickListener() {
+      @Override
+      public boolean onLongClick(View arg0) {
+        int oldValue = buttonClicks.get(key);
+        int newValue = oldValue + 1;
+        buttonClicks.put(key, newValue);
+        return true;
+      }
+    });
+    // Before the click, it should be 0.
+    assertEquals((Integer) 0, buttonClicks.get(key));
+    // Do the click.
+    icon.performClick();
+    assertEquals((Integer) 1, buttonClicks.get(key));
+  }
+
+  @Test
+  public void getView_correctDrawableResource() {
+    View view = this.getView(0, null);
+    ImageView imageView = (ImageView) view.findViewById(R.id.row_item_icon);
+    ShadowDrawable shadow = shadowOf(imageView.getDrawable());
+    org.fest.assertions.api.Assertions.assertThat(
+            shadow.getCreatedFromResId())
+        .isEqualTo(R.drawable.ic_action_settings);
+  }
+
+  @Test
+  public void getView_iconIsPresentAndVisible() {
+    View view = this.getView(0, null);
+    ImageView icon = (ImageView) view.findViewById(R.id.row_item_icon);
+    assertThat(icon)
+      .isNotNull()
+      .isVisible();
+  }
+
+  /**
+   * Retrieve a view from the {@link #mAdapter} from the given position with
+   * the given existingView being recycled. The root view group passed in is
+   * a {@link LinearLayout}.
+   * @param position
+   * @param existingView
+   * @return
+   */
+  private View getView(int position, View existingView) {
+    View result = this.mAdapter.getView(
+        position,
+        existingView,
+        new LinearLayout(Robolectric.application));
+    return result;
+  }
+
+}
---
+++ tables-test/src/org/opendatakit/tables/views/webkits/ControlTest.java
@@ -0,0 +1,413 @@
+package org.opendatakit.tables.views.webkits;
+
+import static org.fest.assertions.api.ANDROID.assertThat;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.robolectric.Robolectric.shadowOf;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.opendatakit.common.android.data.ColumnProperties;
+import org.opendatakit.common.android.data.ColumnType;
+import org.opendatakit.common.android.data.TableProperties;
+import org.opendatakit.tables.activities.AbsBaseActivityStub;
+import org.opendatakit.tables.activities.TableDisplayActivity;
+import org.opendatakit.tables.activities.TableDisplayActivity.ViewFragmentType;
+import org.opendatakit.tables.activities.WebViewActivity;
+import org.opendatakit.tables.utils.Constants;
+import org.opendatakit.tables.utils.IntentUtil;
+import org.opendatakit.tables.utils.ODKDatabaseUtilsWrapper;
+import org.opendatakit.tables.utils.SQLQueryStruct;
+import org.opendatakit.tables.utils.WebViewUtil;
+import org.opendatakit.testutils.TestConstants;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowActivity;
+import org.robolectric.shadows.ShadowActivity.IntentForResult;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.ContentValues;
+
+/**
+ *
+ * @author sudar.sam@gmail.com
+ *
+ */
+@RunWith(RobolectricTestRunner.class)
+public class ControlTest {
+
+  public static final String VALID_STRING_VALUE = "a string value";
+  public static final String VALID_INT_VALUE = "1";
+  public static final String VALID_NUMBER_VALUE = "12.3512";
+
+  Control control;
+  Activity activity;
+  /** The wrapper that should be used by the control stub. */
+  ODKDatabaseUtilsWrapper wrapperMock;
+
+  @Before
+  public void before() {
+    AbsBaseActivityStub activityStub = Robolectric.buildActivity(
+        AbsBaseActivityStub.class)
+          .create()
+          .start()
+          .resume()
+          .visible()
+          .get();
+    // First set up the database utils wrapper mock.
+    this.wrapperMock = mock(ODKDatabaseUtilsWrapper.class);
+    ControlStub.DATABASE = TestConstants.getDatabaseMock();
+    ControlStub.DB_UTILS_WRAPPER = wrapperMock;
+    Control control = new ControlStub(
+        activityStub,
+        TestConstants.TABLES_DEFAULT_APP_NAME);
+    this.activity = activityStub;
+    this.control = control;
+  }
+
+  @After
+  public void after() {
+    AbsBaseActivityStub.resetState();
+    ControlStub.resetState();
+    // null them out just to make sure.
+    this.activity = null;
+    this.control = null;
+  }
+
+  protected void setupControlWithTablePropertiesMock() {
+    TableProperties mock = mock(TableProperties.class);
+    ControlStub.TABLE_PROPERTIES_FOR_ID = mock;
+  }
+
+  @Test
+  public void addRowReturnsFalseIfTableDoesNotExist() {
+    // we don't want the tp to be present
+    ControlStub.TABLE_PROPERTIES_FOR_ID = null;
+    boolean result = this.control.addRow("anyId", "anyString");
+    assertThat(result).isFalse();
+  }
+
+  @Test
+  public void addRowReturnsFalseIfColumnDoesNotExist() {
+    TableProperties tpMock = TestConstants.getTablePropertiesMock();
+    ControlStub.TABLE_PROPERTIES_FOR_ID = tpMock;
+    String nonExistentColumn = "thisColumnDoesNotExist";
+    doReturn(null).when(tpMock).getColumnByElementKey(nonExistentColumn);
+    ControlStub.TABLE_PROPERTIES_FOR_ID = tpMock;
+    String stringifiedMap = WebViewUtil.stringify(getValidMap());
+    boolean result = this.control.addRow("anyId", stringifiedMap);
+    assertThat(result).isFalse();
+  }
+
+  @Test
+  public void addRowWithInvalidIntFails() {
+    this.assertInvalidHelper(ColumnType.INTEGER, "invalid");
+  }
+
+  @Test
+  public void addRowWithInvalidNumberFails() {
+    this.assertInvalidHelper(ColumnType.NUMBER, "invalid");
+  }
+
+  /**
+   * Perform an assertion for an invalid value for the given column type,
+   * ensuring that the insertion fails.
+   * @param elementKey
+   * @param columnType
+   * @param invalidValue
+   */
+  private void assertInvalidHelper(
+      ColumnType columnType,
+      String invalidValue) {
+    setupControlWithTablePropertiesMock();
+    String elementKey = "anyElementKey";
+    TableProperties tpMock = ControlStub.TABLE_PROPERTIES_FOR_ID;
+    ColumnProperties intColumn = TestConstants.getColumnPropertiesMock(
+        elementKey,
+        columnType);
+    doReturn(intColumn).when(tpMock).getColumnByElementKey(
+        elementKey);
+    Map<String, String> invalidMap = new HashMap<String, String>();
+    invalidMap.put(elementKey, invalidValue);
+    String stringified = WebViewUtil.stringify(invalidMap);
+    boolean result = this.control.addRow("anyId", stringified);
+    assertThat(result).isFalse();
+  }
+
+  @Test
+  public void addRowWithValidValuesCallsDBUtilsWrapper() {
+    setupControlWithTablePropertiesMock();
+    TableProperties tpMock = ControlStub.TABLE_PROPERTIES_FOR_ID;
+    ColumnProperties stringColumn = TestConstants.getColumnPropertiesMock(
+        TestConstants.ElementKeys.STRING_COLUMN,
+        ColumnType.STRING);
+    ColumnProperties intColumn = TestConstants.getColumnPropertiesMock(
+        TestConstants.ElementKeys.INT_COLUMN,
+        ColumnType.INTEGER);
+    ColumnProperties numberColumn = TestConstants.getColumnPropertiesMock(
+        TestConstants.ElementKeys.NUMBER_COLUMN,
+        ColumnType.NUMBER);
+    doReturn(stringColumn).when(tpMock).getColumnByElementKey(
+        TestConstants.ElementKeys.STRING_COLUMN);
+    doReturn(intColumn).when(tpMock).getColumnByElementKey(
+        TestConstants.ElementKeys.INT_COLUMN);
+    doReturn(numberColumn).when(tpMock).getColumnByElementKey(
+        TestConstants.ElementKeys.NUMBER_COLUMN);
+    // Now we'll do the call and make sure that we called through to the mock
+    // object successfully.
+    String tableId = "anyTableId";
+    Map<String, String> validMap = getValidMap();
+    String validMapString = WebViewUtil.stringify(validMap);
+    ContentValues contentValues = getContentValuesForValidMap();
+    control.addRow(tableId, validMapString);
+    verify(wrapperMock, times(1)).writeDataIntoExistingDBTable(
+        eq(ControlStub.DATABASE),
+        eq(tableId),
+        eq(contentValues));
+  }
+
+  @Test
+  public void helperLaunchDefaultViewLaunchesCorrectIntent() {
+    this.control.helperLaunchDefaultView(
+        TestConstants.DEFAULT_TABLE_ID,
+        TestConstants.DEFAULT_SQL_WHERE_CALUSE,
+        TestConstants.DEFAULT_SQL_SELECTION_ARGS,
+        TestConstants.DEFAULT_SQL_GROUP_BY,
+        TestConstants.DEFAULT_SQL_HAVING,
+        TestConstants.DEFAULT_SQL_ORDER_BY_ELEMENT_KEY,
+        TestConstants.DEFAULT_SQL_ORDER_BY_DIRECTION);
+    IntentForResult intent = this.getNextStartedIntent();
+    this.assertComponentIsForTableDisplayActivity(
+        intent.intent.getComponent());
+    this.assertDefaultTableIdIsPresent(intent);
+    this.assertFileNameIsNotPresent(intent);
+    this.assertViewFragmentTypeIsPresent(intent, null);
+    this.assertDefaultSQLArgsArePresent(intent);
+    this.assertRowIdIsNotPresent(intent);
+  }
+
+  @Test
+  public void helperOpenTableToSpreadsheetViewLaunchesCorrectIntent() {
+    this.control.helperOpenTableToSpreadsheetView(
+        TestConstants.DEFAULT_TABLE_ID,
+        TestConstants.DEFAULT_SQL_WHERE_CALUSE,
+        TestConstants.DEFAULT_SQL_SELECTION_ARGS,
+        TestConstants.DEFAULT_SQL_GROUP_BY,
+        TestConstants.DEFAULT_SQL_HAVING,
+        TestConstants.DEFAULT_SQL_ORDER_BY_ELEMENT_KEY,
+        TestConstants.DEFAULT_SQL_ORDER_BY_DIRECTION);
+    IntentForResult intent = this.getNextStartedIntent();
+    this.assertComponentIsForTableDisplayActivity(
+        intent.intent.getComponent());
+    this.assertDefaultTableIdIsPresent(intent);
+    this.assertViewFragmentTypeIsPresent(intent, ViewFragmentType.SPREADSHEET);
+    this.assertRowIdIsNotPresent(intent);
+    this.assertFileNameIsNotPresent(intent);
+    this.assertDefaultSQLArgsArePresent(intent);
+  }
+
+  @Test
+  public void helperOpenTableToMapViewLaunchesCorrectIntent() {
+    this.control.helperOpenTableToMapView(
+        TestConstants.DEFAULT_TABLE_ID,
+        TestConstants.DEFAULT_FILE_NAME,
+        TestConstants.DEFAULT_SQL_WHERE_CALUSE,
+        TestConstants.DEFAULT_SQL_SELECTION_ARGS,
+        TestConstants.DEFAULT_SQL_GROUP_BY,
+        TestConstants.DEFAULT_SQL_HAVING,
+        TestConstants.DEFAULT_SQL_ORDER_BY_ELEMENT_KEY,
+        TestConstants.DEFAULT_SQL_ORDER_BY_DIRECTION);
+    IntentForResult intent = this.getNextStartedIntent();
+    this.assertComponentIsForTableDisplayActivity(
+        intent.intent.getComponent());
+    this.assertDefaultTableIdIsPresent(intent);
+    this.assertRowIdIsNotPresent(intent);
+    this.assertViewFragmentTypeIsPresent(intent, ViewFragmentType.MAP);
+    this.assertDefaultFileNameIsPresent(intent);
+    this.assertDefaultSQLArgsArePresent(intent);
+  }
+
+  @Test
+  public void helperOpenTableWithFileLaunchesCorrectIntent() {
+    this.control.helperOpenTableWithFile(
+        TestConstants.DEFAULT_TABLE_ID,
+        TestConstants.DEFAULT_FILE_NAME,
+        TestConstants.DEFAULT_SQL_WHERE_CALUSE,
+        TestConstants.DEFAULT_SQL_SELECTION_ARGS,
+        TestConstants.DEFAULT_SQL_GROUP_BY,
+        TestConstants.DEFAULT_SQL_HAVING,
+        TestConstants.DEFAULT_SQL_ORDER_BY_ELEMENT_KEY,
+        TestConstants.DEFAULT_SQL_ORDER_BY_DIRECTION);
+    IntentForResult intent = this.getNextStartedIntent();
+    this.assertComponentIsForTableDisplayActivity(
+        intent.intent.getComponent());
+    this.assertDefaultTableIdIsPresent(intent);
+    this.assertDefaultFileNameIsPresent(intent);
+    this.assertRowIdIsNotPresent(intent);
+    this.assertViewFragmentTypeIsPresent(intent, ViewFragmentType.LIST);
+    this.assertDefaultSQLArgsArePresent(intent);
+  }
+
+  @Test
+  public void openDetailViewWithFileLaunchesCorrectIntent() {
+    this.control.openDetailViewWithFile(
+        TestConstants.DEFAULT_TABLE_ID,
+        TestConstants.DEFAULT_ROW_ID,
+        TestConstants.DEFAULT_FILE_NAME);
+    IntentForResult intent = this.getNextStartedIntent();
+    this.assertComponentIsForTableDisplayActivity(
+        intent.intent.getComponent());
+    assertThat(intent.intent.getExtras()).isNotNull();
+    this.assertDefaultTableIdIsPresent(intent);
+    this.assertDefaultRowIdIsPresent(intent);
+    this.assertDefaultFileNameIsPresent(intent);
+  }
+
+  @Test
+  public void launchHTMLLaunchesCorrectIntent() {
+    this.control.launchHTML(TestConstants.DEFAULT_FILE_NAME);
+    IntentForResult intent = this.getNextStartedIntent();
+    this.assertComponentIsForWebViewActivity(intent.intent.getComponent());
+    assertThat(intent.intent.getExtras()).isNotNull();
+    this.assertDefaultFileNameIsPresent(intent);
+    this.assertTableIdIsNotPresent(intent);
+    this.assertRowIdIsNotPresent(intent);
+  }
+
+  /**
+   * Get a map with valid values of the map returned by
+   * {@link TestConstants#getMapOfElementKeyToValue(String, String)}.
+   * @return
+   */
+  protected Map<String, String> getValidMap() {
+    Map<String, String> map = TestConstants.getMapOfElementKeyToValue(
+        VALID_STRING_VALUE,
+        VALID_INT_VALUE,
+        VALID_NUMBER_VALUE);
+    return map;
+  }
+
+  protected ContentValues getContentValuesForValidMap() {
+    ContentValues result = new ContentValues();
+    result.put(TestConstants.ElementKeys.STRING_COLUMN, VALID_STRING_VALUE);
+    result.put(
+        TestConstants.ElementKeys.INT_COLUMN,
+        Integer.parseInt(VALID_INT_VALUE));
+    result.put(
+        TestConstants.ElementKeys.NUMBER_COLUMN,
+        Double.parseDouble(VALID_NUMBER_VALUE));
+    return result;
+  }
+
+  private IntentForResult getNextStartedIntent() {
+    ShadowActivity shadowActivity = shadowOf(this.activity);
+    return shadowActivity.peekNextStartedActivityForResult();
+  }
+
+  private void assertComponentIsForTableDisplayActivity(
+      ComponentName component) {
+    ComponentName target = new ComponentName(
+        this.activity,
+        TableDisplayActivity.class);
+    org.fest.assertions.api.Assertions.assertThat(component).isEqualTo(target);
+  }
+
+  private void assertComponentIsForWebViewActivity(ComponentName component) {
+    ComponentName target = new ComponentName(
+        this.activity,
+        WebViewActivity.class);
+    org.fest.assertions.api.Assertions.assertThat(component).isEqualTo(target);
+  }
+
+  private void assertDefaultTableIdIsPresent(IntentForResult intent) {
+    String tableId = IntentUtil.retrieveTableIdFromBundle(
+        intent.intent.getExtras());
+    org.fest.assertions.api.Assertions.assertThat(tableId)
+        .isNotNull()
+        .isEqualTo(TestConstants.DEFAULT_TABLE_ID);
+  }
+
+  private void assertDefaultRowIdIsPresent(IntentForResult intent) {
+    String rowId = IntentUtil.retrieveRowIdFromBundle(
+        intent.intent.getExtras());
+    org.fest.assertions.api.Assertions.assertThat(rowId)
+        .isNotNull()
+        .isEqualTo(TestConstants.DEFAULT_ROW_ID);
+  }
+
+  private void assertRowIdIsNotPresent(IntentForResult intent) {
+    String rowId = IntentUtil.retrieveRowIdFromBundle(
+        intent.intent.getExtras());
+    org.fest.assertions.api.Assertions.assertThat(rowId).isNull();
+  }
+
+  private void assertDefaultFileNameIsPresent(IntentForResult intent) {
+    String fileName = IntentUtil.retrieveFileNameFromBundle(
+        intent.intent.getExtras());
+    org.fest.assertions.api.Assertions.assertThat(fileName)
+        .isNotNull()
+        .isEqualTo(TestConstants.DEFAULT_FILE_NAME);
+  }
+
+  private void assertFileNameIsNotPresent(IntentForResult intent) {
+    String fileName = IntentUtil.retrieveFileNameFromBundle(
+        intent.intent.getExtras());
+    org.fest.assertions.api.Assertions.assertThat(fileName).isNull();
+  }
+
+  private void assertTableIdIsNotPresent(IntentForResult intent){
+    String tableId = IntentUtil.retrieveTableIdFromBundle(
+        intent.intent.getExtras());
+    org.fest.assertions.api.Assertions.assertThat(tableId).isNull();
+  }
+
+  private void assertViewFragmentTypeIsPresent(
+      IntentForResult intent,
+      ViewFragmentType viewFragmentType) {
+    String typeString = intent.intent.getExtras().getString(
+        Constants.IntentKeys.TABLE_DISPLAY_VIEW_TYPE);
+    if (viewFragmentType == null) {
+      org.fest.assertions.api.Assertions.assertThat(typeString).isNull();
+    } else {
+      org.fest.assertions.api.Assertions.assertThat(typeString)
+        .isNotNull()
+        .isEqualTo(viewFragmentType.name());
+    }
+  }
+
+  private void assertDefaultSQLArgsArePresent(IntentForResult intent) {
+    SQLQueryStruct queryStruct = IntentUtil.getSQLQueryStructFromBundle(
+        intent.intent.getExtras());
+    org.fest.assertions.api.Assertions.assertThat(queryStruct).isNotNull();
+    org.fest.assertions.api.Assertions.assertThat(queryStruct.whereClause)
+        .isNotNull()
+        .isEqualTo(TestConstants.DEFAULT_SQL_WHERE_CALUSE);
+    org.fest.assertions.api.Assertions.assertThat(queryStruct.selectionArgs)
+        .isNotNull()
+        .isEqualTo(TestConstants.DEFAULT_SQL_SELECTION_ARGS);
+    org.fest.assertions.api.Assertions.assertThat(queryStruct.groupBy)
+        .isNotNull()
+        .isEqualTo(TestConstants.DEFAULT_SQL_GROUP_BY);
+    org.fest.assertions.api.Assertions.assertThat(queryStruct.having)
+        .isNotNull()
+        .isEqualTo(TestConstants.DEFAULT_SQL_HAVING);
+    org.fest.assertions.api.Assertions.assertThat(
+        queryStruct.orderByElementKey)
+        .isNotNull()
+        .isEqualTo(TestConstants.DEFAULT_SQL_ORDER_BY_ELEMENT_KEY);
+    org.fest.assertions.api.Assertions.assertThat(queryStruct.orderByDirection)
+        .isNotNull()
+        .isEqualTo(TestConstants.DEFAULT_SQL_ORDER_BY_DIRECTION);
+  }
+
+}
